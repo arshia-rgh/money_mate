f202c37020933025c60bc6b3cea484354081c9f1
arshia <arshiarezagholi1212@gmail.com>
2024-08-29 23:30:16 +0330

    added add_expense crud (completed)

--- app/crud/expense.py
+++ app/crud/expense.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from google.cloud import firestore
 
 from app.schemas.expense import Expense
@@ -6,4 +7,12 @@ db = firestore.Client()
 
 
 async def add_expense(expense: Expense, current_user: dict):
-    pass
+    try:
+        expense.user_id = current_user["uid"]
+        expense_ref = db.collection("expenses").document()
+        expense.id = expense_ref.id
+        expense_ref.set(expense.model_dump())
+
+        return {"message": "Expense added successfully", "id": expense.id}
+    except Expense as e:
+        raise HTTPException(status_code=400, detail=str(e))
