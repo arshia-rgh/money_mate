c01ebf39d62cde21988b96301f194b3abddedc31
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 02:12:52 +0330

    registered delete expense to the router

--- app/routers/expense.py
+++ app/routers/expense.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.crud.expense import add_expense
+from app.crud.expense import add_expense, delete_expense, update_expense, get_expense
 from app.dependencies import get_current_user
 from app.schemas.expense import Expense
 
@@ -10,3 +10,8 @@ router = APIRouter()
 @router.post("/add")
 async def add_expense_entry(expense: Expense, current_user: dict = Depends(get_current_user)):
     return await add_expense(expense, current_user)
+
+
+@router.delete("/delete")
+async def delete_expense_entry(expense_id: str, current_user: dict = Depends(get_current_user)):
+    return await delete_expense(expense_id, current_user)
