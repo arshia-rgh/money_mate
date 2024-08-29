e78a17d593610bb95229578c003c4b4e50b6aad5
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 02:05:05 +0330

    added expense update

--- app/crud/expense.py
+++ app/crud/expense.py
@@ -15,6 +15,7 @@ async def add_expense(expense: Expense, current_user: dict):
         expense_ref.set(expense.model_dump())
 
         return {"message": "Expense added successfully", "id": expense.id}
+
     except Expense as e:
         raise HTTPException(status_code=400, detail=str(e))
 
@@ -23,18 +24,32 @@ async def delete_expense(expense_id: str, current_user: dict):
     try:
         expense_ref = db.collection("expenses").document(expense_id)
         expense = expense_ref.get().to_dict()
+
         if expense["user_id"] != current_user["uid"]:
             raise HTTPException(status_code=403,
                                 detail="Permission Denied, You can only delete your expenses not others")
 
         expense_ref.delete()
         return {"message": "Expense deleted successfully"}
+
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
-async def update_expense(expense_id: str, current_user: dict):
-    pass
+async def update_expense(expense_id: str, expense: Expense, current_user: dict):
+    try:
+        expense_ref = db.collection("expenses").document(expense_id)
+        exists_expense = expense_ref.get().to_dict()
+        if exists_expense["user_id"] != current_user["uid"]:
+            raise HTTPException(status_code=403,
+                                detail="Permission Denied, You can only modify your expenses not others")
+
+        expense.user_id = current_user["uid"]
+        expense_ref.update(expense.model_dump())
+        return {"message": "Expense updated successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 async def get_expense(expense_id: str, current_user: dict):
