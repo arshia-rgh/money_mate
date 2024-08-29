8c670c643ca630ccadc98cee7f41f8208ab65b49
arshia <arshiarezagholi1212@gmail.com>
2024-08-29 23:35:12 +0330

    added expense router to the main fastapi app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.routers import user
+from app.routers import user, expense
 
 app = FastAPI()
 
 app.include_router(user.router, prefix="/user")
+app.include_router(expense.router, prefix="/expense")
