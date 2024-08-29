f70c06eabbcdc806a9fef5c4db30547806935d96
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 03:07:23 +0330

    registered all new user cruds to the router

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from app.crud.user import create_user, login_user
+from app.crud.user import create_user, login_user, delete_user, update_user
+from app.dependencies import get_current_user
 from app.schemas.user import UserCreate, UserLogin
 
 router = APIRouter()
@@ -14,3 +15,18 @@ async def signup(user: UserCreate):
 @router.post("/login")
 async def login(user: UserLogin):
     return await login_user(user)
+
+
+@router.delete("/delete")
+async def delete_account(user_id: str, current_user: dict = Depends(get_current_user)):
+    return await delete_user(user_id, current_user)
+
+
+@router.patch("/update")
+async def update_account(user_id: str, user: UserCreate, current_user: dict = Depends(get_current_user)):
+    return await update_user(user_id, user, current_user)
+
+
+@router.get("/profile")
+async def retrieve_account(user_id: str, current_user: dict = Depends(get_current_user)):
+    return await retrieve_account(user_id, current_user)
