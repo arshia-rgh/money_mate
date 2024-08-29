6a0f69635c67ef16db7528734bea78317669c833
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 02:30:13 +0330

    added phone number to the user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, Field, EmailStr
 class UserCreate(BaseModel):
     first_name: Optional[str] = Field(None)
     last_name: Optional[str] = Field(None)
+    phone_number: str = Field(..., max_length=13)
     email: EmailStr = Field(...)
     password: str = Field(..., min_length=8)
 
