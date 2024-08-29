eaeaaa8513dd0680b52761c98a85f9d398c63e7b
arshia <arshiarezagholi1212@gmail.com>
2024-08-29 17:30:22 +0330

    made the User model f name and l name optional and some validations for password and email

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,10 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
 
 
 class User(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    password: str
+    first_name: Optional[str] = Field(None)
+    last_name: Optional[str] = Field(None)
+    email: EmailStr = Field(...)
+    password: str = Field(..., min_length=8)
