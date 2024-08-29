5d1a51c79fde0523db95a22c070a732dd8d175ae
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 01:58:00 +0330

    added delete_expense and permission that only a owner of the expense can delete it

--- app/crud/expense.py
+++ app/crud/expense.py
@@ -19,13 +19,23 @@ async def add_expense(expense: Expense, current_user: dict):
         raise HTTPException(status_code=400, detail=str(e))
 
 
-async def delete_expense(expense: Expense, current_user: dict):
+async def delete_expense(expense_id: str, current_user: dict):
+    try:
+        expense_ref = db.collection("expenses").document(expense_id)
+        expense = expense_ref.get().to_dict()
+        if expense["user_id"] != current_user["uid"]:
+            raise HTTPException(status_code=403,
+                                detail="Permission Denied, You can only delete your expenses not others")
+
+        expense_ref.delete()
+        return {"message": "Expense deleted successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+async def update_expense(expense_id: str, current_user: dict):
     pass
 
 
-async def update_expense(expense: Expense, current_user: dict):
-    pass
-
-
-async def get_expense(expense: Expense, current_user: dict):
+async def get_expense(expense_id: str, current_user: dict):
     pass
