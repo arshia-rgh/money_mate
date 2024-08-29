9524cd8dc7b152be5c72a8e17ba11d7426c13960
arshia <arshiarezagholi1212@gmail.com>
2024-08-29 18:27:20 +0330

    added login top the router

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.crud.user import create_user
-from app.schemas.user import UserCreate
+from app.crud.user import create_user, login_user
+from app.schemas.user import UserCreate, UserLogin
 
 router = APIRouter()
 
@@ -9,3 +9,8 @@ router = APIRouter()
 @router.post("/signup")
 async def signup(user: UserCreate):
     return await create_user(user)
+
+
+@router.post("/login")
+async def login(user: UserLogin):
+    return await login_user(user)
