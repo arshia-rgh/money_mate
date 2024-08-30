117702f9dc0e699060cafa341bc750a6060baa04
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 23:05:37 +0330

    registered retrieve income to the router

--- app/routers/income.py
+++ app/routers/income.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.crud.income import add_income, delete_income, update_income
+from app.crud.income import add_income, delete_income, update_income, retrieve_income
 from app.dependencies import get_current_user
 from app.schemas.income import Income
 
@@ -20,3 +20,8 @@ async def delete_income_entry(income_id: str, current_user: dict = Depends(get_c
 @router.patch("/update/{income_id}")
 async def update_income_entry(income_id: str, income: Income, current_user: dict = Depends(get_current_user)):
     return await update_income(income_id, income, current_user)
+
+
+@router.get("/retrieve/{income_id}")
+async def retrieve_income_entry(income_id: str, current_user: dict = Depends(get_current_user)):
+    return await retrieve_income(income_id, current_user)
