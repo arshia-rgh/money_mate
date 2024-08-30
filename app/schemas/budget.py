a76d1d65fef02489e093c6d873885ce0ceeff526
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 16:58:33 +0330

    added budget schema

---
+++ app/schemas/budget.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class Budget(BaseModel):
+    id: Optional[int] = Field(None)
+    user_id: Optional[int] = Field(None)
+    category: str = Field(...)
+    amount: float = Field(..., max_digits=10, decimal_places=2)
+    start_date: datetime = Field(None)
+    end_date: datetime = Field(None)
+    created_at: Optional[datetime] = Field(None)
+    updated_at: Optional[datetime] = Field(None)
