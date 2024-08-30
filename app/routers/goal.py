da28bf4bb897ea46accea638b52d7f14dd3d85c1
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 23:26:30 +0330

    registered delete goal to the router

--- app/routers/goal.py
+++ app/routers/goal.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.crud.goal import add_goal
+from app.crud.goal import add_goal, delete_goal
 from app.dependencies import get_current_user
 from app.schemas.goal import Goal
 
@@ -10,3 +10,8 @@ router = APIRouter()
 @router.post("/add")
 async def add_goal_entry(goal: Goal, current_user: dict = Depends(get_current_user)):
     return await add_goal(goal, current_user)
+
+
+@router.delete("/delete/{goal_id}")
+async def delete_goal_entry(goal_id: str, current_user: dict = Depends(get_current_user)):
+    return await delete_goal(goal_id, current_user)
