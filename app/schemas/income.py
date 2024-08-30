73575e73d0fb86507eb6ea657b49d09dfb16ecb2
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 16:48:24 +0330

    added Income schema

---
+++ app/schemas/income.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class Income(BaseModel):
+    id: Optional[int] = Field(None)
+    user_id: Optional[int] = Field(None)
+    amount: float = Field(..., max_digits=10, decimal_places=2)
+    source: str = Field(...)
+    date: datetime = Field(...)
+    created_at : Optional[datetime] = Field(None)
+    updated_at: Optional[datetime] = Field(None)
+
