789395d3e81bbcf850ada76146ef79b8a6e44790
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 19:49:01 +0330

    changed prefix from user and expense to users and expenses

--- app/main.py
+++ app/main.py
@@ -4,5 +4,5 @@ from app.routers import user, expense
 
 app = FastAPI()
 
-app.include_router(user.router, prefix="/user")
-app.include_router(expense.router, prefix="/expense")
+app.include_router(user.router, prefix="/users")
+app.include_router(expense.router, prefix="/expenses")
