123908eb492206d49f10b923eb32ffcb107da952
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 23:25:07 +0330

    added delete goal crud

--- app/crud/goal.py
+++ app/crud/goal.py
@@ -22,3 +22,19 @@ async def add_goal(goal: Goal, current_user: dict):
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+async def delete_goal(goal_id: str, current_user: dict):
+    try:
+        goal_ref = firestore_db.collection("goals").document(goal_id)
+        goal = goal_ref.get().to_dict()
+
+        if goal["user_id"] != current_user["uid"]:
+            raise HTTPException(status_code=403, detail="You can only delete your own goal")
+
+        goal_ref.delete()
+
+        return {"message": "The goal has been deleted successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
