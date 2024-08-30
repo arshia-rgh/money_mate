5cc8d01d5cc39cb343cc5820f94ae0d11dd81418
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 19:03:05 +0330

    added delete budget crud

--- app/crud/budget.py
+++ app/crud/budget.py
@@ -20,3 +20,18 @@ async def add_budget(budget: Budget, current_user: dict):
         return {"message": "Budget added successfully", "id": budget.id}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+
+async def delete_budget(budget_id: str, current_user: dict):
+    try:
+        budget_ref = firestore_db.collection("budgets").document(firestore_db)
+        budget = budget_ref.get().to_dict()
+
+        if budget["user_id"] != current_user["uid"]:
+            raise HTTPException(status_code=403, detail="You can only delete your own budget")
+
+        budget_ref.delete()
+
+        return {"message": "The budget has been successfully deleted"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
