a54e493d6bb647824a79070dd394aaf503eed189
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 22:55:12 +0330

    added delete income to the router

--- app/routers/income.py
+++ app/routers/income.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.crud.income import add_income
+from app.crud.income import add_income, delete_income
 from app.dependencies import get_current_user
 from app.schemas.income import Income
 
@@ -10,3 +10,8 @@ router = APIRouter()
 @router.post("/add")
 async def add_income_entry(income: Income, current_user: dict = Depends(get_current_user)):
     return await add_income(income, current_user)
+
+
+@router.delete("/delete/{income_id}")
+async def delete_income_entry(income_id: str, current_user: dict = Depends(get_current_user)):
+    return await delete_income(income_id, current_user)
