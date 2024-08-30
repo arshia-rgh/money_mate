fe476b680e99f2f2a1f986f261c73557307f4bd3
arshia <arshiarezagholi1212@gmail.com>
2024-08-30 19:59:56 +0330

    adde update budget to the router

--- app/routers/budget.py
+++ app/routers/budget.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from app.crud.budget import add_budget, delete_budget
+from app.crud.budget import add_budget, delete_budget, update_budget, list_budget, retrieve_budget
 from app.dependencies import get_current_user
 from app.schemas.budget import Budget
 
@@ -15,3 +15,8 @@ async def add_budget_entry(budget: Budget, current_user: dict = Depends(get_curr
 @router.delete("/delete/{budget_id}")
 async def delete_budget_entry(budget_id: str, current_user: dict = Depends(get_current_user)):
     return await delete_budget(budget_id, current_user)
+
+
+@router.patch("/update/{budget_id}")
+async def update_budget_entry(budget_id: str, budget: Budget, current_user: dict = Depends(get_current_user)):
+    return await update_budget(budget_id, budget, current_user)
