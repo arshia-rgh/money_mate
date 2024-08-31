dd5fdeea0d9d67121b8e6ef05b1adb421182d1fc
arshia <arshiarezagholi1212@gmail.com>
2024-08-31 12:27:45 +0330

    implemented update method

--- app/crud/base.py
+++ app/crud/base.py
@@ -47,8 +47,29 @@ class BaseCRUD(Generic[T]):
         except Exception as e:
             raise HTTPException(status_code=400, detail=str(e))
 
-    async def update_item(self):
-        pass
+    async def update_item(self, item_id: str, item: T, current_user: dict):
+        try:
+            item_ref = firestore_db.collection(f"{self.collection_name}").document(item_id)
+
+            exists_item = item_ref.get().to_dict()
+
+            if not exists_item:
+                raise HTTPException(status_code=404, detail=f"{self.collection_name.capitalize()} not exists")
+
+            if exists_item["user_id"] != current_user["uid"]:
+                raise HTTPException(status_code=403, detail=f"You can only modify your own {self.collection_name}")
+
+            item.user_id = current_user["uid"]
+            item.created_at = exists_item["created_at"]
+            item.updated_at = datetime.now()
+            item.id = item_ref.id
+
+            item_ref.update(item.model_dump())
+
+            return {"message": f"{self.collection_name.capitalize()} has been successfully updated"}
+
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
     async def retrieve_item(self):
         pass
