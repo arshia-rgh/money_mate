2655cd64fe15ce5dfcb623e30bc19c98f28719e8
arshia <arshiarezagholi1212@gmail.com>
2024-08-31 12:50:43 +0330

    removed routers from maain file

--- app/main.py
+++ app/main.py
@@ -1,11 +1,3 @@
 from fastapi import FastAPI
 
-from app.routers import user, expense, budget, income, goal
-
 app = FastAPI()
-
-app.include_router(user.router, prefix="/users")
-app.include_router(expense.router, prefix="/expenses")
-app.include_router(budget.router, prefix="/budgets")
-app.include_router(income.router, prefix="/incomes")
-app.include_router(goal.router, prefix="/goals")
