2ff039a20bc823813a800eb4988e005bb7dbe4ba
arshia <arshiarezagholi1212@gmail.com>
2024-08-31 15:22:58 +0330

    added send mail conf

---
+++ app/utils/send_mail.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi_mail import ConnectionConfig
+
+load_dotenv()
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+    MAIL_TLS=True,
+    MAIL_SSL=False,
+    USE_CREDENTIALS=True,
+)
