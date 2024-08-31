a68e715e6a2f2d217da936780bec01ed72e3afc0
arshia <arshiarezagholi1212@gmail.com>
2024-08-31 13:10:02 +0330

    added all routers to the main app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
-from app.routers import user
+from app.routers import user, items
 
 app = FastAPI()
 app.include_router(user.router, prefix="/users")
+app.include_router(items.expense_router, prefix="/expenses")
+app.include_router(items.budget_router, prefix="/budgets")
+app.include_router(items.goal_router, prefix="/goals")
+app.include_router(items.income_router, prefix="/incomes")
+app.include_router(items.investment_router, prefix="/investments")
