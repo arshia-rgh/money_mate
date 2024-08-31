de80c5e028df8db88d89cadb208c3d41db096223
arshia <arshiarezagholi1212@gmail.com>
2024-08-31 12:55:08 +0330

    added user router again and included to the main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
 
+from app import user_router
+
 app = FastAPI()
+app.include_router(user_router.router, prefix="/users")
---
+++ app/user_router.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends
+
+from app.crud.user import create_user, login_user, delete_user, update_user
+from app.dependencies import get_current_user
+from app.schemas.user import UserCreate, UserLogin
+
+router = APIRouter()
+
+
+@router.post("/signup")
+async def signup(user: UserCreate):
+    return await create_user(user)
+
+
+@router.post("/login")
+async def login(user: UserLogin):
+    return await login_user(user)
+
+
+@router.delete("/delete/{user_id}")
+async def delete_account(user_id: str, current_user: dict = Depends(get_current_user)):
+    return await delete_user(user_id, current_user)
+
+
+@router.patch("/update/{user_id}")
+async def update_account(user_id: str, user: UserCreate, current_user: dict = Depends(get_current_user)):
+    return await update_user(user_id, user, current_user)
+
+
+@router.get("/profile/{user_id}")
+async def retrieve_account(user_id: str, current_user: dict = Depends(get_current_user)):
+    return await retrieve_account(user_id, current_user)
