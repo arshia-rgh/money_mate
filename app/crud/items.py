3d2fade386fcfc7058d1a2bd521d25e3fb1beeb7
arshia <arshiarezagholi1212@gmail.com>
2024-08-31 13:02:38 +0330

    added all crud classes

--- app/crud/items.py
+++ app/crud/items.py
@@ -8,4 +8,21 @@ class ExpenseCRUD(BaseCRUD[expense.Expense]):
         super().__init__(expense.Expense, "expenses")
 
 
+class BudgetCRUD(BaseCRUD[budget.Budget]):
+    def __init__(self):
+        super().__init__(budget.Budget, "budgets")
 
+
+class GoalCRUD(BaseCRUD[goal.Goal]):
+    def __init__(self):
+        super().__init__(goal.Goal, "goals")
+
+
+class IncomeCRUD(BaseCRUD[income.Income]):
+    def __init__(self):
+        super().__init__(income.Income, "incomes")
+
+
+class InvestmentCRUD(BaseCRUD[investment.Investment]):
+    def __init__(self):
+        super().__init__(investment.Investment, "investments")
