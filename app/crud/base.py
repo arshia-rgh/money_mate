f1e571c60fe333c6cdabd1530d6ec5fedee0e615
arshia <arshiarezagholi1212@gmail.com>
2024-08-31 12:11:26 +0330

    added methods to the BaseCrud class

--- app/crud/base.py
+++ app/crud/base.py
@@ -6,4 +6,20 @@ T = TypeVar("T", bound=BaseModel)
 
 
 class BaseCRUD(Generic[T]):
-    pass
+    def __init__(self):
+        pass
+
+    async def add_item(self):
+        pass
+
+    async def delete_item(self):
+        pass
+
+    async def update_item(self):
+        pass
+
+    async def retrieve_item(self):
+        pass
+
+    async def list_items(self):
+        pass
