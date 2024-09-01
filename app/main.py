84a44f97f21b8350bb10c53dbcf59d27890139ee
arshia <arshiarezagholi1212@gmail.com>
2024-09-01 20:57:14 +0330

    added tags for main routers

--- app/main.py
+++ app/main.py
@@ -14,10 +14,10 @@ async def start_notifications(user_id: str, background_tasks: BackgroundTasks):
     return {"message": "Started listening for new notifications"}
 
 
-app.include_router(user.router, prefix="/users")
-app.include_router(items.expense_router, prefix="/expenses")
-app.include_router(items.budget_router, prefix="/budgets")
-app.include_router(items.goal_router, prefix="/goals")
-app.include_router(items.income_router, prefix="/incomes")
-app.include_router(items.investment_router, prefix="/investments")
-app.include_router(items.transaction_router, prefix="/transactions")
+app.include_router(user.router, prefix="/users", tags=["users"])
+app.include_router(items.expense_router, prefix="/expenses", tags=["expenses"])
+app.include_router(items.budget_router, prefix="/budgets", tags=["budgets"])
+app.include_router(items.goal_router, prefix="/goals", tags=["goals"])
+app.include_router(items.income_router, prefix="/incomes", tags=["incomes"])
+app.include_router(items.investment_router, prefix="/investments", tags=["investments"])
+app.include_router(items.transaction_router, prefix="/transactions", tags=["transactions"])
